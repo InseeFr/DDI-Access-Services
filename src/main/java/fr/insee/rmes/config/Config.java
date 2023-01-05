8e767bc4f20d17491f2733a8594d8def84ebb928
Hugo Bouttes <hugo.bouttes@insee.fr>
2023-01-05 15:20:49 +0100

    test sur la config

--- src/main/java/fr/insee/rmes/config/Config.java
+++ src/main/java/fr/insee/rmes/config/Config.java
@@ -1,6 +1,9 @@
 package fr.insee.rmes.config;
 
 
+import lombok.Value;
+import org.apache.log4j.Logger;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
@@ -8,5 +11,8 @@ import org.springframework.context.annotation.ImportResource;
 //@ImportResource({"C:/Users/ZV2AOJ/IdeaProjects/test/DDI-Access-Services/WebContent/WEB-INFapplicationContext.xml"})
 public class Config {
 
+//    private final Logger logger = LogManager.getLogger(Config.class);
+//
+//    @Value()
 
 }
