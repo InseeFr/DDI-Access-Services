156e01f0f7c3858c1bb025d46bc03cf1a5a87313
Jérôme Bourgeois <jerome.bourgeois@insee.fr>
2024-02-21 11:31:19 +0100

    fix: forging attacks vulnerability

--- src/main/java/fr/insee/rmes/ToColecticaApi/service/ColecticaServiceImpl.java
+++ src/main/java/fr/insee/rmes/ToColecticaApi/service/ColecticaServiceImpl.java
@@ -306,6 +306,9 @@ public class ColecticaServiceImpl implements ColecticaService {
     }
 
     private ResponseEntity<String> getStringResponseEntity(String uuid, CloseableHttpClient httpClient) throws ExceptionColecticaUnreachable, JsonProcessingException {
+        if (!uuid.matches("^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-4[0-9a-fA-F]{3}-[89ABab][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$")) {
+            throw new IllegalArgumentException("UUID invalide");
+        }
         HttpGet httpGet = getHttpGet(uuid);
 
         try (CloseableHttpResponse response = httpClient.execute(httpGet)) {
