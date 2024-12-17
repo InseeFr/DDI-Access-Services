d12d803a89e387983ac498b3f4d9a0a09de88b1a
Emmanuel <demey.emmanuel@gmail.com>
2024-12-17 08:22:39 +0000

    feat: add unit test

--- src/main/java/fr/insee/rmes/utils/StringUtils.java
+++ src/main/java/fr/insee/rmes/utils/StringUtils.java
@@ -3,14 +3,10 @@ package fr.insee.rmes.utils;
 import java.util.Arrays;
 
 public class StringUtils {
-	
-	  private StringUtils() {
+	private StringUtils() {
 		    throw new IllegalStateException("Utility class");
 	}
-
 	public static boolean stringContainsItemFromList(String token, String[] list) {
 		return Arrays.stream(list).anyMatch(token::contains);
 	}
-
-
 }
