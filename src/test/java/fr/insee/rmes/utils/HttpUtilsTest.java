0ed930b225a0c3969dc3e5d1a290331c5a02f375
SM <sofiane.maazi@insee.fr>
2025-04-02 08:09:08 +0200

    feature : Sonar issue

--- src/test/java/fr/insee/rmes/utils/HttpUtilsTest.java
+++ src/test/java/fr/insee/rmes/utils/HttpUtilsTest.java
@@ -4,13 +4,10 @@ import org.junit.jupiter.api.Test;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.builder.Input;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpUtilsTest {
+
 
     @Test
     void filterBOM() throws IOException {
@@ -25,16 +22,11 @@ class HttpUtilsTest {
 
 
     @Test
-    void shouldTestIfObjectsHaveBom() {
-        byte[] bom = {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
-        List<byte[]> bytes = List.of("project".getBytes(StandardCharsets.ISO_8859_1),"Léopold".getBytes(StandardCharsets.UTF_8),bom);
-        List<Boolean> hasBom = new ArrayList<>();
-        for(byte[] byteExample : bytes){
-            hasBom.add(byteExample.length > 2
-                    && byteExample[0] == (byte) 0xEF
-                    && byteExample[1] == (byte) 0xBB
-                    && byteExample[2] == (byte) 0xBF);
-        }
-        assertEquals(List.of(false,false,true),hasBom);
+    void shouldTestIfObjectsHaveBom() throws IOException {
+        byte[] filteredContentWithBom= HttpUtilsTest.class.getResourceAsStream("/utf8-bom/fichierAvecBom.xml").readAllBytes();
+        byte[] filteredContentWithoutBom= HttpUtilsTest.class.getResourceAsStream("/utf8-bom/fichierSansBom.xml").readAllBytes();
+        boolean hasBomFilteredContentWithBom = filteredContentWithBom.length > 2 && filteredContentWithBom[0] == (byte) 0xEF && filteredContentWithBom[1] == (byte) 0xBB && filteredContentWithBom[2] == (byte) 0xBF;
+        boolean hasBomFilteredContentWithoutBom = filteredContentWithoutBom.length > 2 && filteredContentWithoutBom[0] == (byte) 0xEF && filteredContentWithoutBom[1] == (byte) 0xBB && filteredContentWithoutBom[2] == (byte) 0xBF;
+        assertTrue(hasBomFilteredContentWithBom & !hasBomFilteredContentWithoutBom );
     }
 }
