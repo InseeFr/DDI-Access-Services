5f262dc563c8f6133499dd9e2599b9857312256c
SM <sofiane.maazi@insee.fr>
2025-04-02 14:04:39 +0200

    feature : RmesExceptionIOTest.java

--- src/test/java/fr/insee/rmes/exceptions/RmesExceptionIOTest.java
+++ src/test/java/fr/insee/rmes/exceptions/RmesExceptionIOTest.java
@@ -1,18 +1,24 @@
 package fr.insee.rmes.exceptions;
 
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RmesExceptionIOTest {
 
+
     @Test
-    void shouldReturnRestMessageObject() {
-        int status =401;
-        String message= "Absent rights";
-        String details = "Invalid password or tocken";
-        RmesExceptionIO rmesExceptionIO= new RmesExceptionIO(status,message,details);
-        RestMessage restMessage= rmesExceptionIO.toRestMessage();
-        assertNotNull(restMessage.toString());
+    void shouldReturnRestMessageObjectParametersPossibilities() {
+
+        ArrayList<Integer> status = new ArrayList<>(Arrays.asList(ErrorCodes.LINK_CREATION_RIGHTS_DENIED,-1));
+        ArrayList<String> messages = new ArrayList<>(Arrays.asList("Unauthorized",null));
+        ArrayList<String> details = new ArrayList<>(Arrays.asList("Invalid password or tocken",null));
+
+        RmesExceptionIO rmesExceptionIOExampleOne= new RmesExceptionIO(status.getFirst(), messages.getFirst(), details.getFirst());
+        RmesExceptionIO rmesExceptionIOExampleTwo= new RmesExceptionIO(status.getLast(),messages.getLast(),details.getLast());
+
+        assertTrue(rmesExceptionIOExampleOne.getMessage()!=null && rmesExceptionIOExampleTwo.getMessage()==null);
     }
 
 }
