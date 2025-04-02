c1963f9a605d3c2eef2ec0f825aac9bba542da0e
SM <sofiane.maazi@insee.fr>
2025-04-02 07:55:41 +0200

    feature : add test in HttpUtilsTest.java

--- src/test/java/fr/insee/rmes/utils/HttpUtilsTest.java
+++ src/test/java/fr/insee/rmes/utils/HttpUtilsTest.java
@@ -3,8 +3,10 @@ package fr.insee.rmes.utils;
 import org.junit.jupiter.api.Test;
 import org.xmlunit.builder.DiffBuilder;
 import org.xmlunit.builder.Input;
-
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,5 +21,20 @@ class HttpUtilsTest {
                 .ignoreWhitespace()
                 .build();
         assertFalse(diff.hasDifferences(), diff.toString());
+    }
+
+
+    @Test
+    void shouldTestIfObjectsHaveBom() throws IOException{
+        byte[] bom = {(byte) 0xEF, (byte) 0xBB, (byte) 0xBF};
+        List<byte[]> bytes = List.of("project".getBytes(StandardCharsets.ISO_8859_1),"Léopold".getBytes(StandardCharsets.UTF_8),bom);
+        List<Boolean> hasBom = new ArrayList<>();
+        for(byte[] byteExample : bytes){
+            hasBom.add(byteExample.length > 2
+                    && byteExample[0] == (byte) 0xEF
+                    && byteExample[1] == (byte) 0xBB
+                    && byteExample[2] == (byte) 0xBF);
+        }
+        assertEquals(List.of(false,false,true),hasBom);
     }
 }
