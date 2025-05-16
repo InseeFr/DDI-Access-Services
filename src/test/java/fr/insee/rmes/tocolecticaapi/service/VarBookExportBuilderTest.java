45bb50d6dbaf74688d3565d01055ad5be27822a5
SM <sofiane.maazi@insee.fr>
2025-05-16 15:07:46 +0200

    feature: Sonar issues

--- src/test/java/fr/insee/rmes/tocolecticaapi/service/VarBookExportBuilderTest.java
+++ src/test/java/fr/insee/rmes/tocolecticaapi/service/VarBookExportBuilderTest.java
@@ -9,8 +9,8 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 class VarBookExportBuilderTest {
 
     @ParameterizedTest
-    @ValueSource(strings = { " ", "---","<?xml version=\"1.0\"\n" +"<!DOCTYPE article PUBLIC>\n"  + " <para> example</para>\n" })
-    void shouldReturnRmesException(String exampleOfString) {
+    @ValueSource(strings = { " ", "---","<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n <!DOCTYPE article PUBLIC>\n <article lang=\"\">\n <para>This text is an example of what can be done with XML. It's truly amazing.</para>\n </article>" })
+    void getData(String exampleOfString) {
         VarBookExportBuilder varBookExportBuilder = new VarBookExportBuilder();
         RmesException exception =assertThrows(RmesException.class, () -> varBookExportBuilder.getData(exampleOfString));
         assertTrue(exception.getDetails().contains("Can't parse xml"));
