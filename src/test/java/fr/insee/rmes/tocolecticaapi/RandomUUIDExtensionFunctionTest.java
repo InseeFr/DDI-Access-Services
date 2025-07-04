e5fd0f81dbca68aefdf8c67dd05b0827333e719e
SM <sofiane.maazi@insee.fr>
2025-07-04 08:57:57 +0200

    feature : method for RandomUUIDExtensionFunctionTest.java

--- src/test/java/fr/insee/rmes/tocolecticaapi/RandomUUIDExtensionFunctionTest.java
+++ src/test/java/fr/insee/rmes/tocolecticaapi/RandomUUIDExtensionFunctionTest.java
@@ -1,7 +1,9 @@
 package fr.insee.rmes.tocolecticaapi;
 
-import net.sf.saxon.s9api.SaxonApiException;
+import net.sf.saxon.s9api.*;
 import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.Objects;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class RandomUUIDExtensionFunctionTest {
@@ -13,4 +15,15 @@ class RandomUUIDExtensionFunctionTest {
         assertTrue(sequence.length==5 && sequence[0].length()==8 && sequence[1].length()==4 && sequence[2].length()==4 && sequence[3].length()==4 && sequence[4].length()==12 );
     }
 
+    @Test
+    void shouldReturnAttributesWhenRandomUUIDExtensionFunction() {
+        RandomUUIDExtensionFunction random = new RandomUUIDExtensionFunction();
+
+        boolean isEqualGetName = Objects.equals(random.getName().toString(), "randomUUID");
+        boolean isEqualGetResultType = random.getResultType().toString()!=null;
+        boolean isEqualGetArgumentTypes = Objects.equals(Arrays.toString(random.getArgumentTypes()), "[]");
+
+        assertTrue(isEqualGetResultType && isEqualGetArgumentTypes && isEqualGetName);
+
+    }
 }
