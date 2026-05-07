d11264943ad10a355e53cd0944d59ce9f0871f4a
GtanSndil <mail@ad.insee.intra>
2026-05-07 09:32:03 +0200

    tests: imports and others

--- src/test/java/fr/insee/rmes/utils/XpathProcessorImplTest.java
+++ src/test/java/fr/insee/rmes/utils/XpathProcessorImplTest.java
@@ -82,7 +82,7 @@ class XpathProcessorImplTest {
     @Test
     void shouldReturnSAXParseExceptionWhenWrongXmlEntered() {
         SAXParseException exception = assertThrows(SAXParseException.class, () -> xpathProcessor.toDocument("Fake mocked Xml Or Fragment"));
-        assertEquals("Content is not allowed in prolog.",exception.getMessage());
+        assertEquals("Contenu non autorisé dans le prologue.",exception.getMessage());
     }
 
 }
